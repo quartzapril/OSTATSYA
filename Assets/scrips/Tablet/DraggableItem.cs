bb49643bd7c957111c36f2168c3b9d4fb673e276
cherryka-07 <arinakozlovich10610@gmail.com>
2026-01-22 10:26:24 +1000

    добавила игру Таблетки

---
+++ Assets/scrips/Tablet/DraggableItem.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [HideInInspector] public Transform parentAfterDrag;
+    private Image image;
+    private Transform originalParent;
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        originalParent = transform.parent;
+        originalPosition = transform.localPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Запоминаем текущего родителя
+        parentAfterDrag = transform.parent;
+
+        // Поднимаем объект на верхний уровень Canvas
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+
+        // Отключаем raycast, чтобы видеть слоты под объектом
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Двигаем объект за курсором
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Включаем raycast обратно
+        image.raycastTarget = true;
+
+        // Устанавливаем нового родителя (если слот изменился)
+        transform.SetParent(parentAfterDrag);
+
+        // Если родитель не изменился - возвращаем на место
+        if (parentAfterDrag == originalParent)
+        {
+            transform.localPosition = originalPosition;
+        }
+        else
+        {
+            // Центрируем в новом слоте
+            transform.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/scrips/Tablet/DropSlot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+    public string requiredTag = "PillPink";
+
+    // Просто проверяем, есть ли уже дети у слота
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        if (dropped == null) return;
+
+        // Проверяем тег таблетки
+        if (dropped.tag != requiredTag) return;
+
+        // Проверяем, есть ли уже таблетка в этом слоте
+        if (transform.childCount > 0)
+        {
+            return;
+        }
+
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        if (draggableItem != null)
+        {
+            draggableItem.parentAfterDrag = transform;
+        }
+    }
+}
